b91798d2c018a64d7eeb27c73dcf83550742efc2
HENRIQUE BATISTA <85231417+henbatista@users.noreply.github.com>
2023-08-12 16:59:26 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 function Home() {
-    return <h1>Insista, persista e nunca desista.🤝💪 Junior eu amo você, se você ja deu a bundinha dê uma risadinha</h1> 
+  return (
+    <h1>
+      Insista, persista e nunca desista.🤝💪 Junior eu amo você, se você ja deu
+      a bundinha dê uma risadinha
+    </h1>
+  );
+}
+
+function test() {
+  console.log("test");
 }
 
 export default Home;
