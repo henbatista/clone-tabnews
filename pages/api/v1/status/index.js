3e6d9ed3e41799f630e2429f6227be14e9471d90
HENRIQUE BATISTA <85231417+henbatista@users.noreply.github.com>
2024-07-29 17:34:07 +0000

    create

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,57 @@
 import database from "infra/database.js";
 
+async function getMaxConnections() {
+  const query = {
+    text: "SHOW max_connections;",
+  };
+  const result = await database.query(query);
+  return result.rows[0].max_connections;
+}
+
+async function getOpenConnections() {
+  const query = {
+    text: "SELECT COUNT(*) AS opened_connections FROM pg_stat_activity;",
+  };
+
+  const result = await database.query(query);
+  return result.rows[0].opened_connections;
+}
+
+async function getVersion() {
+  const query = {
+    text: "SHOW server_version;",
+  };
+
+  const result = await database.query(query);
+  return result.rows[0].server_version;
+}
+
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "são acima da média" });
+  const updatedAt = new Date().toISOString();
+
+  try {
+    const maxConnections = await getMaxConnections();
+    console.log("maxConnections", maxConnections);
+
+    const openedConnections = await getOpenConnections();
+    console.log("openedConnections", openedConnections);
+
+    const version = await getVersion();
+    console.log("version", version);
+
+    response.status(200).json({
+      updated_at: updatedAt,
+      max_connections: maxConnections,
+      opened_connections: openedConnections,
+      version: version,
+    });
+  } catch (error) {
+    console.error("Erro ao obter status:", error);
+
+    response.status(500).json({
+      error: "Erro ao obter status",
+    });
+  }
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,21 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parseUpdatedAt);
+
+  expect(responseBody.max_connections).toBeDefined();
+  expect(Number.isInteger(parseInt(responseBody.max_connections))).toBe(true);
+
+  expect(responseBody.opened_connections).toBeDefined();
+  expect(Number.isInteger(parseInt(responseBody.opened_connections))).toBe(
+    true,
+  );
+
+  expect(responseBody.version).toBeDefined();
+  expect(typeof responseBody.version).toBe(`string`);
 });
